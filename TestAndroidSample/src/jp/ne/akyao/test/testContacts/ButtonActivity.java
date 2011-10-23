909e2c062b9bdac50f887566b6ee34f905729f41
akyao <konama.akyao@gmail.com>
2011-10-23 11:54:31 +0900

    initial commit

---
+++ TestAndroidSample/src/jp/ne/akyao/test/testContacts/ButtonActivity.java
@@ -0,0 +1,54 @@
+package jp.ne.akyao.test.testContacts;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ButtonActivity extends Activity {
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case R.id.contacts:
+			Intent intent = new Intent(this, ContactsActivity.class);
+			intent.setAction(Intent.ACTION_VIEW);
+			startActivity(intent);
+			break;
+		case R.id.button:
+			break;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button);
+        Button button = (Button) findViewById(R.id.buttonHoge);
+        button.setOnClickListener(new MyOnclickListener());
+    }
+    
+    private  class MyOnclickListener implements OnClickListener{
+    	@Override
+    	public void onClick(View v) {
+    		Toast.makeText(ButtonActivity.this, "Clicked", Toast.LENGTH_SHORT);    	
+    	}
+    }
+}
---
+++ TestAndroidSample/src/jp/ne/akyao/test/testContacts/ContactsActivity.java
@@ -0,0 +1,103 @@
+package jp.ne.akyao.test.testContacts;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ContactsActivity extends Activity {
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case R.id.contacts:
+			break;
+		case R.id.button:
+			Intent intent = new Intent(this, ButtonActivity.class);
+			intent.setAction(Intent.ACTION_VIEW);
+			startActivity(intent);
+			break;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        
+        Cursor cursor = managedQuery(ContactsContract.Contacts.CONTENT_URI,
+        		null, null, null, ContactsContract.Contacts._ID + " desc");
+        
+        int count = 0;
+        while(cursor.moveToNext()){
+        	//1コンタクトごとにループ
+        	
+        	//電話帳全件回すとエラーが発生するので、件数制限
+        	count++;
+        	if(count > 20){
+        		break;
+        	}
+        	
+        	//コンタクトのIDと表示名を取得
+        	String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+        	String name = cursor.getString(cursor.getColumnIndex("display_name"));
+        	String strContact = id + " " + name;
+        	
+        	//電話番号を取得
+        	String phones = fetch(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        			ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,
+        			ContactsContract.CommonDataKinds.Phone.NUMBER);
+        	strContact = strContact + phones;
+        	
+        	//Email情報を取得
+        	String emails = fetch(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+        			ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + id,
+        			ContactsContract.CommonDataKinds.Email.DATA);
+        	strContact = strContact + emails;
+        	
+        	adapter.add(strContact);
+        }
+        cursor.close();
+        
+        ListView listview = (ListView) findViewById(R.id.listview);
+        
+        listview.setAdapter(adapter);
+    }
+    
+    private String fetch(Uri uri, String selection, String columnName){
+    	Cursor emails = managedQuery(uri,
+    			null,
+    			selection,
+    			null,
+    			null);
+    	
+    	String content = "";
+    	while(emails.moveToNext()){
+    		String data = emails.getString(emails.getColumnIndex(columnName));
+    		content = content + "/n" + data;
+    	}
+    	
+    	emails.close();
+    	return content;
+    }
+}
