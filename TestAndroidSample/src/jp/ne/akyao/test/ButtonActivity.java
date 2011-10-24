d189e303c1e7dddc31e7d01d13da37e28f28fc25
akyao <konama.akyao@gmail.com>
2011-10-24 10:41:52 +0900

    package変更

--- TestAndroidSample/src/jp/ne/akyao/test/ButtonActivity.java
+++ TestAndroidSample/src/jp/ne/akyao/test/ButtonActivity.java
@@ -1,6 +1,7 @@
-package jp.ne.akyao.test.testContacts;
+package jp.ne.akyao.test;
 
 
+import jp.ne.akyao.test.testContacts.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
